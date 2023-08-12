0c937e374c24122d84c6aa910f7ea61d85cbe1f2
Ignacio Nava <ignan_90@hotmail.com>
2023-08-12 10:46:38 -0300

    Static files sistem modified

--- core/settings.py
+++ core/settings.py
@@ -123,10 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static_dev/'
+STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static_dev"),
 )
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
